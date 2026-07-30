346a8a915fdd9a809403c8c9e77c6f8fb2d8847f
Alexis Ayala <donelson07@hotmail.com>
2026-07-30 09:00:03 -0600

    Publish static export to GitHub Pages (fixed slug lengths)

---
+++ _next/static/chunks/app/page-3d5fe7b78ff7c792.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[168,234,334,974],{8368:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8500,23))}},e=>{e.O(0,[500,441,794,358],()=>e(e.s=8368)),_N_E=e.O()}]);
